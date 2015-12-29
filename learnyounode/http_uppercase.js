6c41e6a791456041bdf07dc0509859916b4f982c
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2015-12-29 15:13:41 +0900

    Complate http_uppercase.js

---
+++ learnyounode/http_uppercase.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  if (req.method != 'POST') {
+    return res.end('send me a POST\n');
+  }
+  var map = require('through2-map');
+  req.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(res);
+});
+server.listen(process.argv[2]);
